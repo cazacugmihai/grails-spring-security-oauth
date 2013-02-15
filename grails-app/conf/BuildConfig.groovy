006df01019734caa7249758d1ec811dbc5099f23
enr <enrico.comiti@gmail.com>
2013-02-15 22:54:27 +0100

    fix compilation problem with no-Groovy2 Spock version

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -24,11 +24,14 @@ grails.project.dependency.resolution = {
         compile "org.springframework.social:spring-social-core:1.0.2.RELEASE"
         compile "org.springframework.social:spring-social-facebook:1.0.1.RELEASE"
         compile "org.springframework.social:spring-social-twitter:1.0.2.RELEASE"
+        runtime "org.scribe:scribe:1.3.2"
     }
 
     plugins {
-        compile ":spring-security-core:1.2.7.3",
-                ":oauth:2.2.1"
+        compile ":spring-security-core:1.2.7.3"
+        compile ":oauth:2.2.1", {
+            transitive = false
+        }
 
         build ":release:2.2.0", ":rest-client-builder:1.0.3", { 
             export = false 
